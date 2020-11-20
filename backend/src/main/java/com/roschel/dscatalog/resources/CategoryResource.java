a6859aa843e9b1e1ae2c3e408ff85ab20e903abd
roschel <joaomroschel@gmail.com>
2020-11-20 08:14:21 -0300

    category dto

--- backend/src/main/java/com/roschel/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/roschel/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,6 @@
 package com.roschel.dscatalog.resources;
 
-import com.roschel.dscatalog.entities.Category;
+import com.roschel.dscatalog.dto.CategoryDTO;
 import com.roschel.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +18,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
