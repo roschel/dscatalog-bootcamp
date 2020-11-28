c597aaf2de75d3f3a21e6ef0391f129fa03e1779
roschel <joaomroschel@gmail.com>
2020-11-27 23:55:40 -0300

    DELETE Category

--- backend/src/main/java/com/roschel/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/roschel/dscatalog/services/CategoryService.java
@@ -3,8 +3,11 @@ package com.roschel.dscatalog.services;
 import com.roschel.dscatalog.dto.CategoryDTO;
 import com.roschel.dscatalog.entities.Category;
 import com.roschel.dscatalog.repository.CategoryRepository;
+import com.roschel.dscatalog.services.exceptions.DatabaseException;
 import com.roschel.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -54,6 +57,17 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
 
     }
