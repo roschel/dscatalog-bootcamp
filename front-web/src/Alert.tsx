b27335b2f0634e5e80f738be1875d82feed52089
roschel <joaomroschel@gmail.com>
2020-12-03 22:12:20 -0300

    criando front

---
+++ front-web/src/Alert.tsx
@@ -0,0 +1,16 @@
+import { type } from 'os';
+import React from 'react';
+
+type Props={
+    text?: string;
+}
+
+const Alert = ({text}:Props) => {
+    return (
+    <div className="alert alert-primary">
+        Hello {text}
+    </div>
+    )
+}
+
+export default Alert;
---
+++ front-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Alert from './Alert';
+
+const App = () =>{
+    return(
+        <div className="container">
+            <Alert />
+        </div>
+    )
+}
+
+export default App;
