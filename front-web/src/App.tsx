e068b9ead80ca9f1d4e0727249398f84b96195e9
roschel <joaomroschel@gmail.com>
2020-12-03 22:39:33 -0300

    useState, conditional rendering and useEffect

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import Alert from './Alert';
+import React, { useState, useEffect } from 'react';
 
-const App = () =>{
-    return(
-        <div className="container">
-            <Alert />
+const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log('compnente iniciado')
+    }, [])
+
+
+
+    return (
+        <div className="container mt-5">
+            <button
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1)}
+            >
+                +
+            </button>
+
+            <span>
+                {counter}
+            </span>
+
+            <button
+                className="btn btn-primary ml-5"
+                onClick={() => setCounter(counter - 1)}
+            >
+                -
+            </button>
+
+            {counter > 5 && <h1>O valor é maior que 5</h1>}
+            {counter <= 5 && <h1>O valor é menor ou igual que 5</h1>}
         </div>
     )
 }
