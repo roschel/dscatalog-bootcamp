9fc4d0721ae98153438b0bcd00dee19306ac9906
roschel <joaomroschel@gmail.com>
2020-12-08 22:13:56 -0300

    adding backend and listing products

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -23,7 +23,7 @@ const Catalog = () => {
 
         const params = {
             page: 0,
-            linesPerPage: 5
+            linesPerPage: 12
         }
 
         makeRequest({ url: '/products', params })
@@ -36,8 +36,8 @@ const Catalog = () => {
             <h1 className="catalog-title">Catálogo de produtos</h1>
             <div className="catalog-products">
                 {productsResponse?.content.map(product => (
-                    <Link to="/products/1" key={product.id}>
-                        <ProductCard />
+                    <Link to={`/products/${product.id}`} key={product.id}>
+                        <ProductCard product={product}/>
                     </Link>
                 ))}
             </div>
