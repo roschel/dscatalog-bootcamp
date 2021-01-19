33dd1462eb14d653e00c2f601cc0698c8a87eb77
roschel <joaomroschel@gmail.com>
2021-01-18 22:22:27 -0300

    Testing services delete with exception

--- backend/src/test/java/com/roschel/dscatalog/tests/services/ProductServiceTests.java
+++ backend/src/test/java/com/roschel/dscatalog/tests/services/ProductServiceTests.java
@@ -2,6 +2,7 @@ package com.roschel.dscatalog.tests.services;
 
 import com.roschel.dscatalog.repositories.ProductRepository;
 import com.roschel.dscatalog.services.ProductService;
+import com.roschel.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,15 @@ public class ProductServiceTests {
 
         Mockito.doNothing().when(repository).deleteById(existingId);
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
     }
 
     @Test
