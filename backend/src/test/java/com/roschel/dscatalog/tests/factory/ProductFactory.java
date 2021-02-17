0c43fa9062e375f5982fd12a637f4587f4b1746d
roschel <joaomroschel@gmail.com>
2021-02-17 00:32:25 -0300

    Creating ProductResourceTests and refacting ProductFactory

--- backend/src/test/java/com/roschel/dscatalog/tests/factory/ProductFactory.java
+++ backend/src/test/java/com/roschel/dscatalog/tests/factory/ProductFactory.java
@@ -1,24 +1,36 @@
 package com.roschel.dscatalog.tests.factory;
 
 import com.roschel.dscatalog.dto.ProductDTO;
+import com.roschel.dscatalog.entities.Category;
 import com.roschel.dscatalog.entities.Product;
 
 import java.time.Instant;
 
 public class ProductFactory {
     public static Product createProduct(){
-        return new Product(
+        Product product =  new Product(
                 1L,
                 "Phone",
                 "Good Phone",
                 800.0,
                 "https://img.com/img;png",
-                Instant.parse("2021-10-20T03:00:00Z")
+                Instant.parse("2021-01-20T03:00:00Z")
         );
+
+        product.getCategories().add(new Category(1L,null));
+
+        return product;
     }
 
     public static ProductDTO createproductDTO(){
-        return new ProductDTO(createProduct());
-    }
+        Product product = createProduct();
 
+        return new ProductDTO(product, product.getCategories());
+    }
+    
+    public static ProductDTO createproductDTO(Long id){
+        ProductDTO dto = createproductDTO();
+        dto.setId(id);
+        return dto;
+    }
 }
