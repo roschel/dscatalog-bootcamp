07fa2a02a84b09f0a20c03a9d13f2da9921ffeee
João Roschel <joaomroschel@gmail.com>
2021-03-02 10:52:28 -0300

    centralizando pagina de login

--- front-web/src/pages/Auth/Components/Login/index.tsx
+++ front-web/src/pages/Auth/Components/Login/index.tsx
@@ -1,5 +1,5 @@
 import ButtonIcon from 'core/components/ButtonIcon'
-import React, { Children } from 'react'
+import React from 'react'
 import {useForm} from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import AuthCard from '../Card'
